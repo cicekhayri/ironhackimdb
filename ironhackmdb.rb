a5baa2f2d1484d1ed90f56e09a1e4f9628b6e831
Hayri <hayri@hoshilab.com>
2014-09-25 21:28:34 +0200

    Get IMDB info first then save to database

--- ironhackmdb.rb
+++ ironhackmdb.rb
@@ -8,45 +8,55 @@ require 'sinatra/reloader'
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: 'ironhackmdb.sqlite'
+  database: 'ironhackmdb_premium.sqlite'
 )
 
 class TVShow < ActiveRecord::Base
 
   # search the IMDB database
-  def new_imdb_object(argument)
-    @search_for_movie = Imdb::Search.new(argument).movies.first
-    @the_series = Imdb::Serie.new(@search_for_movie.id)
-  end
+  #def new_imdb_object(argument)
+  #  search_for_movie = Imdb::Search.new(argument).movies.first
+  #  the_series = Imdb::Serie.new(search_for_movie.id)
+  #end
 
-  # we have name, own_rating and own_comments available
+  # we have name, own_rating and own_comments available
   validates :name, presence: true,uniqueness: true, length: { in: 1..10 }
   validates :own_comments, length: { in: 100..1000 }
   validates :own_rating, presence: true, numericality: true 
+  before_create :get_imdb_info_save_to_db
  
-  def imdb_rating
-    @the_serie = new_imdb_object(name).rating
-  end
+  
+#  def imdb_rating
+#    the_serie = get_imdb_info_save_to_db(name).rating
+#  end
 
-  def imdb_number_of_seasons
-    @the_serie = new_imdb_object(name).seasons.size
-  end
+#  def imdb_number_of_seasons
+#    the_serie = get_imdb_info_save_to_db(name).seasons.size
+#  end
 
-  def imdb_get_movie_poster
-    @the_serie = new_imdb_object(name).poster
-  end
+#  def imdb_get_movie_poster
+#    the_serie = get_imdb_info_save_to_db(name).poster
+#  end
 
-  def get_imdb_id
-    @the_serie = new_imdb_object(name).id
+#  def get_imdb_id
+#    the_serie = get_imdb_info_save_to_db(name).id
+#  end
+
+  private
+  def get_imdb_info_save_to_db
+    search_for_movie = Imdb::Search.new(name).movies.first
+    the_series = Imdb::Serie.new(search_for_movie.id)
+    self.imdb_rating = the_series.rating
+    self.imdb_number_of_seasons = the_series.seasons.size
+    self.imdb_link = the_series.url
+    self.imdb_picture_link = the_series.poster
   end
 end
 
 
 get '/' do
-  @tv_shows = TVShow.order('id DESC')
-  puts "="
-  puts @tv_shows
-  puts "="
+  @tv_shows = TVShow.order(id: :desc)
+  #@tv_shows = TVShow.all
   erb :index
 end
 
@@ -55,9 +65,14 @@ post '/' do
   the_show.name = params[:name]
   the_show.own_rating = params[:own_rating]
   the_show.own_comments = params[:own_comments]
-
-  the_show.save
-  redirect '/'
+  
+  if the_show.save
+    redirect '/'
+  else
+    the_show.errors.each do |e|
+      puts e
+    end  
+  end
 end
 
 get '/our-rating/:id' do
--- spec/ironhackmdb_spec.rb
+++ spec/ironhackmdb_spec.rb
@@ -6,6 +6,8 @@ describe TVShow do
     @tvshow.name = "Friends"
     @tvshow.own_rating = 6
     @tvshow.own_comments = "Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor" 
+    search_for_movie = Imdb::Search.new(@tvshow.name).movies.first
+    @the_series = Imdb::Serie.new(search_for_movie.id)
   end
 
   it "should not be empty string in name field" do
@@ -21,14 +23,15 @@ describe TVShow do
   end
 
   it "should have a method which gets the IMDB rating for the movie" do
-    expect(@tvshow.imdb_rating).to eql(9.0)
+    expect(@the_series.rating).to eql(9.0)
   end
 
   it "should have a method which gets the IMDB seasons for the serie" do
-    expect(@tvshow.imdb_number_of_seasons).to eql(10)
+    expect(@the_series.seasons.size).to eql(10)
   end
 
   it "should have a method which gets the IMDB poster for the movie" do
-    expect(@tvshow.imdb_get_movie_poster).to eql("http://ia.media-imdb.com/images/M/MV5BMzgxMzcxMjYzMl5BMl5BanBnXkFtZTcwMzE3NTEyMQ@@.jpg")
+    expect(@the_series.poster).to eql("http://ia.media-imdb.com/images/M/MV5BMzgxMzcxMjYzMl5BMl5BanBnXkFtZTcwMzE3NTEyMQ@@.jpg")
   end
+
 end
